7b4b4cb1cd78d223bfb682b2acc609a943a49463
dvora-ba <dvora0742@gmail.com>
2023-06-21 14:49:24 +0300

    add route

---
+++ app.js
@@ -0,0 +1,11 @@
+const express=require('express')
+
+ const app=express();
+     
+ const userRoutes = require('./UserRouter');
+   
+app.use(userRoutes)
+
+ const port = process.env.PORT || 3000;
+
+ app.listen(3000, () => console.log('app is listening at port 3000'));
