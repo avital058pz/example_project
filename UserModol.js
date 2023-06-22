a335040171e9246adc132c3e9e68be414501993e
אביטל פינץ <avital058pz@gmail.com>
2023-06-22 11:42:21 +0300

    change

--- UserModol.js
+++ UserModol.js
@@ -1,3 +1,7 @@
+
+const axios = require('axios');
+
+
 class User{
     constructor(userID,name,email,poneNumber){
         this.userID=userID
@@ -6,16 +10,35 @@ class User{
         this.poneNumber=poneNumber
       
     }
-  
+
+
+
+
+    Validation(){
+       if(this.name==null ||this.email==null || this.poneNumber==null) {
+        throw new Error("All fields are mandatory!")
+       }
+       if(!this.email.includes('@')||this.email.includes(' '))
+          throw new Error("Invalid email!")
+
+    
+       
+    }
+    
 }
 const UsersArr=[];
 const user1=new User(2,"shira","batziongreen3320","0548493320");
 UsersArr.push(user1)
 function createUser(userID,name,email,poneNumber){
+    Validation();
     const user2=new User(userID,name,email,poneNumber);
+
     UsersArr.push(user2)
     return user2
-}
+};
+
+
+
 function getallusers(){
     return UsersArr
 }
@@ -28,11 +51,13 @@ function updateUser(userID,name,email,poneNumber){
 }
 function deleteUser(userID){
     const index=UsersArr.findIndex((x)=>x.userID==userID)
-    UsersArr.splice(index,1)//תמחוק במקום הindex אלמנט אחד
+    UsersArr.splice(index,1) 
 
 }
 function getUserbyId(userID){
     const user=UsersArr.find((x)=>x.userID==userID) 
+      if(!user)
+          throw new Error('user not found')
     return user
 }
 module.exports={
--- userController.js
+++ userController.js
@@ -1,30 +1,38 @@
-const data=require("./UserModol")
+const data = require("./UserModol")
 
-const Controler={
-    getAllUser:(req,res)=>{
-        res.status(200).json(data.getallusers());
-    },
-    createUser:(req,res)=>{
-      const {userID,name,email,poneNumber}=req.body;
-      res.status(200).json(data.createUser(userID,name,email,poneNumber));
-    },
-    
- 
-    updateUser:(req,res)=>{
-      const {userID,name,email,poneNumber}=req.body
-      res.status(200).json(data.updateUser(userID,name,email,poneNumber));
-    },
-    deleteUser:(req,res)=>{
-         const  id =req.params.userId;
-         data.deleteUser(id); 
-         res.status(200).json({ message: 'deleted' });
-    },
-    getUserbyId:(req,res)=>{
-           const  id =req.params.userId;
-           res.status(200).json(data.getUserbyId(id));
+const Controler = {
+  getAllUser: (req, res) => {
+    res.status(200).json(data.getallusers());
+  },
+  createUser: (req, res) => {
+    const { userID, name, email, poneNumber } = req.body;
+    res.status(200).json(data.createUser(userID, name, email, poneNumber));
+  },
 
 
-    },
+  updateUser: (req, res) => {
+    const { userID, name, email, poneNumber } = req.body
+    res.status(200).json(data.updateUser(userID, name, email, poneNumber));
+  },
+  deleteUser: (req, res) => {
+    const id = req.params.userId;
+    data.deleteUser(id);
+    res.status(200).json({ message: 'deleted' });
+  },
+  getUserbyId: (req, res) => {
+
+    const id = req.params.userId;
+    try {
+      const user = data.getUserbyId(id)
+      if (!user) {
+       return res.status(404).json("not found")
+      }
+    res.status(200).json(user)
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+  },
 };
 
-module.exports=Controler;
+module.exports = Controler;
